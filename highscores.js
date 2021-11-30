a41e55f8450b16a301d5ea977d8c37f22bf8b463
deluus polycarp <pdeluus@gmail.com>
2021-11-29 16:32:24 -0800

    hw4

--- highscores.js
+++ highscores.js
@@ -33,7 +33,7 @@ console.log(highScores)
 
 for (var i = 0; i < highScores.length; i ++){
     var li = document.createElement("li")
-    li.innerHTML = highScores[i].initials, highScores[i].score;
+    li.innerHTML = highScores[i].initials + " " + highScores[i].score;
     ul.append(li)
     
    } 
